a6c0b81d31bc556b27f9ddc318cdb6ee94e63e83
johnmfwee <johnmfwee@gmail.com>
2021-10-22 09:18:56 +1100

    add initial python files

---
+++ lists.py
@@ -0,0 +1,13 @@
+# This is a python comment 
+names = ["Harry", "Ron", "Hermione"]
+# Print the list
+print(names)
+# Print a specific element of the list
+# Remember always starts with 0
+print(names[1])
+#Add a new name to the lists
+names.append("Draco")
+#Sort the lists
+names.sort()
+#Print the new list
+print(names)
