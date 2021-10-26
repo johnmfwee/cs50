cef5190ca0f6af1f35a550678718c551e7ce11ed
johnmfwee <johnmfwee@gmail.com>
2021-10-27 10:49:55 +1100

    more files

---
+++ dict.py
@@ -0,0 +1,14 @@
+# Define a dictionary
+houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
+# Print out Harry's house
+print(houses["Harry"])
+# Add values to dictionary
+houses["Hermione"] = "Gryffindor"
+# Print Hermione's house
+print(houses["Hermione"])
+
+"""
+    Output:
+        Gryffindor
+        Gryffindor
+"""
---
+++ loops.py
@@ -0,0 +1,7 @@
+for i in range(6):
+    print(i)
+# New List
+names = ["Harry", "Ron", "Hermione"]
+# print new list
+for name in names:
+    print(name)
